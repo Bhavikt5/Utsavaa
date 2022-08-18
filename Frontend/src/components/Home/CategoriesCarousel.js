a993788217b3ee6b4863b1d890fa2b2c401bf485
bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2022-08-18 10:29:24 +0530

    categoriesdata file created and updated

--- Frontend/src/components/Home/CategoriesCarousel.js
+++ Frontend/src/components/Home/CategoriesCarousel.js
@@ -9,6 +9,7 @@ import "swiper/css/pagination";
 
 // import required modules
 import { Pagination } from "swiper";
+import { categoriesData } from "./categoriesData";
 
 const CategoriesCarousel = () => {
   return (
@@ -22,67 +23,20 @@ const CategoriesCarousel = () => {
         modules={[Pagination]}
         className="mySwiper"
       >
-        <SwiperSlide>
-          <a href="/products/crystals">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652706860/utsavaa/cfbwisfatlilim9mbro9.png"
-              alt="First slide"
-            />
-            <h1>Crystals</h1>
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="/products/bracelets">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652706785/utsavaa/jkycypyckefby44orpot.png"
-              alt="Second slide"
-            />
-            <h1>Bracelets</h1>
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="/malas">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652698983/utsavaa/xtt56smmsg3s17kmncqe.png"
-              alt="Third slide"
-            />
-            <h1>Malas</h1>
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="/Candles">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652706860/utsavaa/cfbwisfatlilim9mbro9.png"
-              alt="Third slide"
-            />
-            <h1>Candles</h1>
-          </a>
-        </SwiperSlide>
-        <SwiperSlide>
-          <a href="/book">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652796797/utsavaa/jd5je6fnh5e6y2md1utq.png"
-              alt="Third slide"
-            />
-            <h1>Book</h1>
-          </a>
-        </SwiperSlide>
-
-        <SwiperSlide>
-          <a href="/hamper">
-            <img
-              className="d-block w-100"
-              src="https://res.cloudinary.com/duormjcef/image/upload/v1652659886/utsavaa/te6grsuathpsguvcmzzc.png"
-              alt="Third slide"
-            />
-            <h1>Hamper</h1>
-          </a>
-        </SwiperSlide>
+        {categoriesData.map((i) => {
+          return (
+            <SwiperSlide>
+              <a href={i.link}>
+                <img
+                  className="d-block w-100"
+                  src={i.image}
+                  alt="First slide"
+                />
+                <h1>{i.title}</h1>
+              </a>
+            </SwiperSlide>
+          );
+        })}
       </Swiper>
     </Fragment>
   );
---
+++ Frontend/src/components/Home/categoriesData.js
@@ -0,0 +1,38 @@
+export const categoriesData = [
+  {
+    link: "/products/crystals",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652706860/utsavaa/cfbwisfatlilim9mbro9.png",
+    title: "Crystals",
+  },
+  {
+    link: "/products/bracelets",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652706785/utsavaa/jkycypyckefby44orpot.png",
+    title: "Bracelets",
+  },
+  {
+    link: "/products/malas",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652698983/utsavaa/xtt56smmsg3s17kmncqe.png",
+    title: "Malas",
+  },
+  {
+    link: "/products/candles",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652706860/utsavaa/cfbwisfatlilim9mbro9.png",
+    title: "Candles",
+  },
+  {
+    link: "/products/book",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652796797/utsavaa/jd5je6fnh5e6y2md1utq.png",
+    title: "Book",
+  },
+  {
+    link: "/products/hamper",
+    image:
+      "https://res.cloudinary.com/duormjcef/image/upload/v1652659886/utsavaa/te6grsuathpsguvcmzzc.png",
+    title: "Hamper",
+  },
+];
